f253d889a6ffdc60be29f43527b872f8961b5f0c
Bao Vuong <vqbao@lv5.org>
2015-05-10 22:55:45 -0400

    it can do more things. nice.

--- article.py
+++ article.py
@@ -1,6 +1,6 @@
 from ConfigParser import ConfigParser
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -41,5 +41,29 @@ browser.find_element_by_id('j_username').send_keys(username)
 browser.find_element_by_id('j_password').clear()
 browser.find_element_by_id('j_password').send_keys(password)
 browser.find_element_by_id('formSubmit').click()
+
+
+assert 'Web of Science' in browser.title
+# ok, prompt for topic
+topic = raw_input('what topic do you want to search for? ')
+
+# type topic into input
+browser.find_element_by_id('value(input1)').send_keys(topic)
+browser.find_element_by_link_text('+ Add Another Field').click()
+browser.find_element_by_id('value(input2)').send_keys('Journal of the American Chemical Society')
+selection = browser.find_element_by_id('select2')
+for option in selection.find_elements_by_tag_name('option'):
+    if option.text == 'Publication Name':
+        option.click()
+        break
+browser.find_element_by_id('value(input2)').send_keys(Keys.RETURN)
+#browser.find_element_by_id('UA_GeneralSearch_input_form_sb').click()
+
+
+
+
+#assert 'pdf' in browser.current_url
+#browser.find_element_by_id('download')
+
 
 #browser.quit()
